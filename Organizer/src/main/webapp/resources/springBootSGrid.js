65208663e60e99d20d889a0607d9ef465ed972d5
goworekp <goworekp@L0801.tt.com.pl>
2017-12-14 15:46:08 +0100

    Void attempt to write clear js code

--- Organizer/src/main/webapp/resources/springBootSGrid.js
+++ Organizer/src/main/webapp/resources/springBootSGrid.js
@@ -1,2 +1,19 @@
 
+$.ajax({
+  url: "/allUsagesModel",
+  type:"POST",
+  dataType: "json",
+  success: function( result ) {
+	  $.each(data, function(ind, obj){
+			
+			$("#tableClient").row.add([
+				obj.issueID,
+				"<input type='checkbox' value='"+obj.issueID+"' id=''>",
+				obj.issueName,
+				obj.issueMemo,
+				obj.issuePriority
+			]).draw();
+	  })
+  }
+		});
 
